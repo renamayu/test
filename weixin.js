33e1505c09e8ec84d98e5d2fd12cb272ffa9553d
lucky <myy326@qq.com>
2016-09-08 15:21:10 +0800

    wx

--- weixin.js
+++ weixin.js
@@ -392,13 +392,56 @@ wx.ready(function () {
     wx.showOptionMenu();
   };
 
+
+
+// 基本类
+
+// 举报: "menuItem:exposeArticle"
+// 调整字体: "menuItem:setFont"
+// 日间模式: "menuItem:dayMode"
+// 夜间模式: "menuItem:nightMode"
+// 刷新: "menuItem:refresh"
+// 查看公众号（已添加）: "menuItem:profile"
+// 查看公众号（未添加）: "menuItem:addContact"
+// 传播类
+
+// 发送给朋友: "menuItem:share:appMessage"
+// 分享到朋友圈: "menuItem:share:timeline"
+// 分享到QQ: "menuItem:share:qq"
+// 分享到Weibo: "menuItem:share:weiboApp"
+// 收藏: "menuItem:favorite"
+// 分享到FB: "menuItem:share:facebook"
+// 分享到 QQ 空间/menuItem:share:QZone
+// 保护类
+
+// 编辑标签: "menuItem:editTag"
+// 删除: "menuItem:delete"
+// 复制链接: "menuItem:copyUrl"
+// 原网页: "menuItem:originPage"
+// 阅读模式: "menuItem:readMode"
+// 在QQ浏览器中打开: "menuItem:openWithQQBrowser"
+// 在Safari中打开: "menuItem:openWithSafari"
+// 邮件: "menuItem:share:email"
+// 一些特殊公众号: "menuItem:share:brand"
+
+
   // 8.3 批量隐藏菜单项
   document.querySelector('#hideMenuItems').onclick = function () {
     wx.hideMenuItems({
       menuList: [
         'menuItem:readMode', // 阅读模式
         'menuItem:share:timeline', // 分享到朋友圈
-        'menuItem:copyUrl' // 复制链接
+        'menuItem:copyUrl', // 复制链接
+        'menuItem:share:QZone',
+        'menuItem:share:weiboApp',
+        'menuItem:favorite',
+        'menuItem:share:qq',
+        'menuItem:share:QZone',
+        'menuItem:share:email',
+        'menuItem:readMode',
+        'menuItem:originPage',
+        'menuItem:openWithQQBrowser',
+        'menuItem:openWithSafari',
       ],
       success: function (res) {
         alert('已隐藏“阅读模式”，“分享到朋友圈”，“复制链接”等按钮');
