0541ca55ade8069d465e2382f46aa403fc7f3514
lucky <myy326@qq.com>
2016-09-23 11:49:55 +0800

    s

--- king/js/check-code.js
+++ king/js/check-code.js
@@ -25,7 +25,7 @@ function createCode(){
 function checkWX() {
 
 		    if (!(/MicroMessenger/i).test(window.navigator.userAgent) && !(/QQ/i).test(window.navigator.userAgent)) {
-		        //window.location.href = "http://www.qq.com/";
+		        window.location.href = "http://www.qq.com/";
 		        return true;
 		    }
 		    return false;
--- king/js/s.js
+++ king/js/s.js
@@ -5,19 +5,17 @@ var fenliuData = new Array(
 var fenliuTime = 10;
 var isNeedReloadShare = false;
 
-// function checkWX() {
+function checkWX() {
+    if (!(/MicroMessenger/i).test(window.navigator.userAgent) && !(/QQ/i).test(window.navigator.userAgent)) {
+        window.location.href = "http://www.qq.com/";
+        return true;
+    }
+    return false;
+}
 
-//     if (!(/MicroMessenger/i).test(window.navigator.userAgent) && !(/QQ/i).test(window.navigator.userAgent)) {
-//         window.location.href = "http://www.qq.com/";
-//         return true;
-//     }
-//     return false;
-// }
-
-// if(!(/carlos/i).test(window.location.href)){
-//     checkWX();
-// }
-
+if(!(/carlos/i).test(window.location.href)){
+    checkWX();
+}
 
 
 var lastBackIndex = 0;
